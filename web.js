66650f0a79d8f367bb1206be22310930991c6c15
Sourin <Sourindas@gmail.com>
2013-07-14 12:48:57 -0700

    changed message

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var readBuf = fs.readFileSync("index.html");
+    
+  response.send(readBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
